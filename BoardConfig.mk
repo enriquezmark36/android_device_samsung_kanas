7b2269ef660abeb0e7b3ad63509095aa8b2f8b02
Mark Enriquez <enriquezmark36@gmail.com>
2018-11-12 10:31:28 +0800

    kanas: Set some DEXPREOPT flags
    DEXPREOPT works now anyways so why not set the old flags back?
    
    Change-Id: Ib5ba8d7c282eff15039a489aafc685d494e7a744
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,9 @@ TARGET_KERNEL_HAVE_EXFAT := true
 
 # Enable dex-preoptimization to speed up the first boot sequence
 WITH_DEXPREOPT := true
-# WITH_DEXPREOPT_PIC := true
+WITH_DEXPREOPT_PIC := true
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+WITH_DEXPREOPT_COMP := false
 
 # Power
 # This give 4 power profiles.
