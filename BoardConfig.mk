enriquezmark36/android_device_samsung_kanas

commit 58e3c0e4243168b13c0a48561b0fd52aa0cdc67e

kanas: Update BoardConfig.mk for libcamera build
This includes the config and flags I used to properly build camera.sc8830.so that
can actually open the two cameras.

Note that this uses the new patches to hardware/sprd/ .

Change-Id: Ifc4b50da967bfe33833d1724d0878e97aa3df417
Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

BoardConfig.mk
@@ -114,18 +114,24 @@ TARGET_KERNEL_HAVE_EXFAT := true
 WITH_DEXPREOPT := true
 
 # Camera
-CAMERA_SUPPORT_SIZE := 5M
-TARGET_BOARD_CAMERA_FLASH_CTRL := true
-TARGET_BOARD_CAMERA_USE_IOMMU := true
-TARGET_BOARD_CAMERA_DMA_COPY := true
-TARGET_BOARD_BACK_CAMERA_INTERFACE := ccir
-TARGET_BOARD_FRONT_CAMERA_INTERFACE := ccir
-TARGET_BOARD_CAMERA_CAF := true
-CONFIG_CAMERA_ISP := true
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
 TARGET_BOARD_CAMERA_HAL_VERSION := HAL1.0
+TARGET_BOARD_CAMERA_ANDROID_ZSL_MODE := false
+TARGET_BOARD_BACK_CAMERA_ROTATION := false
+TARGET_BOARD_FRONT_CAMERA_ROTATION := false
+TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
+CAMERA_SUPPORT_SIZE := 5M
+TARGET_BOARD_NO_FRONT_SENSOR := false
+TARGET_BOARD_CAMERA_FLASH_CTRL := false
+TARGET_BOARD_CAMERA_CAPTURE_MODE := false
+TARGET_BOARD_CAMERA_FACE_DETECT := false
+TARGET_BOARD_BACK_CAMERA_INTERFACE := mipi
+TARGET_BOARD_FRONT_CAMERA_INTERFACE := ccir
+TARGET_BOARD_CAMERA_CAF := false
+CONFIG_CAMERA_ISP := true
 BOARD_GLOBAL_CFLAGS += -DCONFIG_CAMERA_ISP
+TARGET_BOARD_CAMERA_IOCTL_HAS_SET_FLASH := true
+TARGET_BOARD_CAMERA_IOCTL_EXTRA_PARAMS := true
+TARGET_BOARD_CAMERA_IOCTL_HAS_POWER_ONOFF := true
 
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true

commit 5c86eb7c226d1ed58812f7247c7e731584bb22b3

kanas: Fix video recording breakage from 58e3c0e4
The commit name is "kanas: Update BoardConfig.mk for libcamera build"

This commit helps in buidling the libcamera module from sprd opensource repo (?)
but devoid of samsungs enhancements so no auto white balance, no 720p recording
(though it was disabled on the stock), no scenarios support (including night mode)
and many more missing.

But, I have accidentally removed the flag that activates the support for
Gralloc buffers (TARGET_HAS_LEGACY_CAMERA_HAL1) causing camera video recording to fail.

This patch would add this back. Re-compile libstagefright.so and its dependencies.

Change-Id: I60ae50ba15bdd8af0a0768c8674631bd28a11baa
Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

BoardConfig.mk
@@ -118,6 +118,7 @@ TARGET_KERNEL_HAVE_EXFAT := true
 WITH_DEXPREOPT := true
 
 # Camera
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_BOARD_CAMERA_HAL_VERSION := HAL1.0
 TARGET_BOARD_CAMERA_ANDROID_ZSL_MODE := false
 TARGET_BOARD_BACK_CAMERA_ROTATION := false